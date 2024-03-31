901f73fe3ce026f456e8ebda0b0d3175f9a3f484
Shivam Saxena <shivam2003official@gmail.com>
2024-03-31 22:19:28 +0530

    just to maintain streak

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,14 +1,12 @@
 import { createContext, useContext } from "react";
 
-
-
 export const UserContext = createContext({
-    color: '#44bbff',
-    colorName: 'skyblue',
+	color: "#44bbff",
+	colorName: "skyblue",
 });
 
 export const ContextProvider = UserContext.Provider;
 
-export default function useUserContext(){
-    return useContext(UserContext);
+export default function useUserContext() {
+	return useContext(UserContext);
 }
